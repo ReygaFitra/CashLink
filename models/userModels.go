2e2cfc89dad4a144c7d3469df3bd194f7779476f
ReygaFitra <regafitra12@gmail.com>
2023-06-27 10:21:35 +0700

    models:add username

--- models/userModels.go
+++ models/userModels.go
@@ -6,6 +6,7 @@ type User struct {
 	gorm.Model
 	ID       int64    `gorm:"primaryKey;autoIncrement" json:"id"` 
 	Name     string  `gorm:"not null" json:"name"`
+	Username     string  `gorm:"unique;not null" json:"username"`
 	Email    string `gorm:"unique" json:"email"`
 	Password string `gorm:"not null" json:"password"`
 }
