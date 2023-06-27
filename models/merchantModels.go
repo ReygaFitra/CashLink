ae12f59b5c584a99477e57668f9cc6825adc7dcc
ReygaFitra <regafitra12@gmail.com>
2023-06-27 11:32:53 +0700

    models:add merchant table

---
+++ models/merchantModels.go
@@ -0,0 +1,17 @@
+package models
+
+type Merchant struct {
+	Merchant_ID       int64     `gorm:"primaryKey;autoIncrement" json:"merchant_id"`
+	Merchant_Name     string    `gorm:"not null" json:"merchant_name"`
+	Merchant_Email    string    `gorm:"unique" json:"merchant_email"`
+	Merchant_Password string    `gorm:"not null" json:"merchant_password"`
+	Merchant_Address  string    `gorm:"not null" json:"merchant_address"`
+	Products          []Product `gorm:"foreignKey:MerchantID"`
+}
+
+type Product struct {
+	Product_ID    int64   `gorm:"primaryKey;autoIncrement" json:"product_id"`
+	Product_Name  string  `gorm:"not null" json:"product_name"`
+	Product_Price float64 `gorm:"not null" json:"product_price"`
+	MerchantID    int64   `gorm:"not null" json:"merchant_id"`
+}
