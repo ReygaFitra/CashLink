612969844ec34d888f599d46bafcfd654e31ea02
ReygaFitra <regafitra12@gmail.com>
2023-06-28 14:24:28 +0700

    models:user->add amount column

--- models/userModels.go
+++ models/userModels.go
@@ -1,9 +1,10 @@
 package models
 
 type User struct {
-	ID       int64    `gorm:"primaryKey;autoIncrement" json:"id"` 
-	Name     string  `gorm:"not null" json:"name"`
-	Username     string  `gorm:"unique;not null" json:"username"`
+	ID       int64  `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name     string `gorm:"not null" json:"name"`
+	Username string `gorm:"unique;not null" json:"username"`
 	Email    string `gorm:"unique" json:"email"`
 	Password string `gorm:"not null" json:"password"`
+	Amount   int64  `json:"amount"`
 }
