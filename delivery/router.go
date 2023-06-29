b8f1cb86dea11cdd72176f95010b29e60067915e
ReygaFitra <regafitra12@gmail.com>
2023-06-29 17:26:56 +0700

    fix:endpoint router

--- delivery/router.go
+++ delivery/router.go
@@ -2,6 +2,7 @@ package delivery
 
 import (
 	"github.com/ReygaFitra/CashLink.git/controllers"
+	"github.com/ReygaFitra/CashLink.git/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,24 +13,24 @@ func RunServer() {
 	r.POST("/signup/user", controllers.SignUp)
 	r.POST("/login/user", controllers.Login)
 	r.POST("/user/logout",  controllers.Logout)
-	r.GET("/user", controllers.AuthMiddleware, controllers.ViewUser)
-	r.PUT("/user/:id", controllers.AuthMiddleware, controllers.UpdateUser)
-	r.GET("/user/search/:username", controllers.AuthMiddleware, controllers.FindUserByName)
+	r.GET("/user/:userID", middleware.AuthMiddleware, controllers.ViewUser)
+	r.PUT("/user/:userID", middleware.AuthMiddleware, controllers.UpdateUser)
+	r.GET("/user/search/:userID/:username", middleware.AuthMiddleware, controllers.FindUserByName)
 
 	// Merchant Authentication
 	r.POST("/signup/merchant", controllers.RegisterMerchant)
 	r.POST("/login/merchant", controllers.LoginMerchant)
 	r.POST("/merchant/logout",  controllers.Logout)
-	r.GET("/merchant", controllers.MerchantMiddleware, controllers.ViewMerchant)
+	r.GET("/merchant/:merchantID", middleware.MerchantMiddleware, controllers.ViewMerchant)
 	// Merchant Products
-	r.POST("/merchant/product", controllers.MerchantMiddleware, controllers.AddProduct)
-	r.PUT("/merchant/product", controllers.MerchantMiddleware, controllers.UpdateProduct)
-	r.GET("/merchant/product", controllers.MerchantMiddleware, controllers.ViewProducts)
+	r.POST("/merchant/product/:merchantID", middleware.MerchantMiddleware, controllers.AddProduct)
+	r.PUT("/merchant/product/:merchantID", middleware.MerchantMiddleware, controllers.UpdateProduct)
+	r.GET("/merchant/product/:merchantID", middleware.MerchantMiddleware, controllers.ViewProducts)
 
 	// Transaction
-	r.POST("user/transaction/transfer/:senderID/:recipientID", controllers.AuthMiddleware, controllers.Transfer)
-	r.POST("user/transaction/payment/:userID/:merchantID/:productID", controllers.AuthMiddleware, controllers.Payment)
-	r.GET("user/transaction/history/:userID", controllers.AuthMiddleware, controllers.HistoryTransaction)
+	r.POST("user/transaction/transfer/:userID/:recipientID", middleware.AuthMiddleware, controllers.Transfer)
+	r.POST("user/transaction/payment/:userID/:merchantID/:productID", middleware.AuthMiddleware, controllers.Payment)
+	r.GET("user/transaction/history/:userID", middleware.AuthMiddleware, controllers.HistoryTransaction)
 	
 	r.Run()
 }
