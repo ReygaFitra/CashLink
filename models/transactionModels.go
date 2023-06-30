a06fdfc0297758254c9e6ed6dd6e33b9cc6bb187
ReygaFitra <regafitra12@gmail.com>
2023-06-30 13:53:08 +0700

    models:remove transfer log & payment log models

--- models/transactionModels.go
+++ models/transactionModels.go
@@ -1,43 +1,21 @@
 package models
 
-import "time"
-
 type Transfer struct {
-	ID         uint       `gorm:"primaryKey"`
-	SenderID   uint       `gorm:"not null" json:"sender_id"`
-	ReceiverID uint       `gorm:"not null" json:"receiver_id"`
-	Amount     float64    `gorm:"not null" json:"amount"`
-	Sender     User       `gorm:"foreignKey:SenderID"`
-	Receiver   User       `gorm:"foreignKey:ReceiverID"`
-}
-
-type TransferLog struct {
-	TransferLog_ID          int64     `gorm:"primaryKey;autoIncrement" json:"tf_log_id"`
-	TransferLog_Amount      float64   `gorm:"not null" json:"tf_log_amount"`
-	TransferLog_SenderID    int64     `gorm:"not null" json:"tf_log_sender_id"`
-	TransferLog_RecipientID int64     `gorm:"not null" json:"tf_log_recipient_id"`
-	TransferLog_Description string    `gorm:"not null" json:"tf_log_description"`
-	CreatedAt   time.Time `gorm:"not null" json:"timestamp"`
-	Transfer                Transfer  `gorm:"foreignKey:TransferLog_ID"`
+	ID         uint    `gorm:"primaryKey"`
+	SenderID   uint    `gorm:"not null" json:"sender_id"`
+	ReceiverID uint    `gorm:"not null" json:"receiver_id"`
+	Amount     float64 `gorm:"not null" json:"amount"`
+	Sender     User    `gorm:"foreignKey:SenderID"`
+	Receiver   User    `gorm:"foreignKey:ReceiverID"`
 }
 
 type Payment struct {
-	Payment_ID            uint     `gorm:"primaryKey;autoIncrement"`
-	Payment_UserID        uint     `gorm:"not null" json:"payment_userid"`
-	Payment_MerchantID    uint     `gorm:"not null" json:"payment_merchantid"`
-	Payment_ProductID     uint     `gorm:"not null" json:"payment_productid"`
-	Payment_Amount        float64   `gorm:"not null"`
-	User               User      `gorm:"foreignKey:Payment_UserID"`
-	Merchant           Merchant  `gorm:"foreignKey:Payment_MerchantID"`
-	Product            Product   `gorm:"foreignKey:Payment_ProductID"`
-}
-
-type PaymentLog struct {
-	PaymentLog_ID          int64     `gorm:"primaryKey;autoIncrement" json:"pay_log_id"`
-	PaymentLog_UserID      int64     `gorm:"not null" json:"pay_log_user_id"`
-	PaymentLog_MerchantID  int64     `gorm:"not null" json:"pay_log_merchant_id"`
-	PaymentLog_ProductID   int64     `gorm:"not null" json:"pay_log_product_id"`
-	PaymentLog_Amount      float64   `gorm:"not null" json:"pay_log_amount"`
-	CreatedAt     time.Time `gorm:"not null" json:"timestamp"`
-	Payment               Payment   `gorm:"foreignKey:PaymentLog_ID"`
+	Payment_ID         uint     `gorm:"primaryKey;autoIncrement"`
+	Payment_UserID     uint     `gorm:"not null" json:"payment_userid"`
+	Payment_MerchantID uint     `gorm:"not null" json:"payment_merchantid"`
+	Payment_ProductID  uint     `gorm:"not null" json:"payment_productid"`
+	Payment_Amount     float64  `gorm:"not null"`
+	User               User     `gorm:"foreignKey:Payment_UserID"`
+	Merchant           Merchant `gorm:"foreignKey:Payment_MerchantID"`
+	Product            Product  `gorm:"foreignKey:Payment_ProductID"`
 }
